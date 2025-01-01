8b8450fff075ab4946ec9c2815617264953d2d06
David <dasolma@gmail.com>
2025-01-01 11:58:35 +0100

    remove unneeded code

--- src/phm_framework/nets/callbacks.py
+++
@@ -1,7 +0,0 @@
-from phm_framework.nets.metrics import FewShotMetric
-
-def fewshot_extra_callbacks(context):
-    model = context['model']
-    val_few_shot_gen = context['data_generators']['few_shot_val']
-
-    return [FewShotMetric((val_few_shot_gen,), model)]
